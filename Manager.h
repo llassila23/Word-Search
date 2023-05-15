b2456f289127cc3ff1751850924968aaefe3c9d5
Lucas Lassila <129896722+llassila23@users.noreply.github.com>
2023-05-15 12:51:18 -0700

    Add files via upload
    This version has the functions for creating the data storage array and the word search array. It has a method to fill the data array from user input. This method has a bug where it begins to fill the word search array instead of stopping after the data array has been filled. It has something to do with the inheritance or the for loop control. It currently does not perform as expected. Finally there is a for each loop method to print the data storage array to console.

--- Manager.h
+++ Manager.h
@@ -5,4 +5,38 @@
 #ifndef ASSIGNMENT_1_WORD_SEARCH_MANAGER_H
 #define ASSIGNMENT_1_WORD_SEARCH_MANAGER_H
 // methods defined here
+#include <iostream>
+
+using namespace std;
+
+class Manager {
+public:
+    // constructor, value initialization of array word[]
+
+        string word[4] = {};
+        char matrix[6][6] = {}; // array that can hold four words
+
+};
+
+class WordSearch : public Manager{ // inherit the arrays from manager
+    public:
+
+     void getWords(){
+        cout<< "please enter a 2 - 6 letter word when prompted"<< endl;
+        cout<< "these will be the words in the word search"<< endl;
+        for(int i = 0; i<= sizeof(word); i++){
+            cout<< "enter a word"<< endl;
+            cin >> word[i];
+        }
+    }
+
+    void print(){
+        for(string val : word){
+            cout<< val << endl;
+        }
+    }
+
+
+
+};
 #endif //ASSIGNMENT_1_WORD_SEARCH_MANAGER_H
--- main.cpp
+++ main.cpp
@@ -4,15 +4,25 @@
 // assignment 1 Word Search
 // This program is meant to take words from a user and put them in a word search
 
+#include "Manager.h"
+using namespace std;
+int main(){
+    WordSearch newWS;
+    newWS.getWords();
+    newWS.print();
+}
+
+
 /* prototyping begins here:
 header file with bulk of methods
 
 instructions/menu
  take user input of words. start with 3
  put each word in array list (object?)
- build an 2 dimenaional array, nxn of chars.
+ build an 2-dimensional array, nxn of chars.
  arrange words in to this array
-    traverse one row. if start character of a string is contained in that row. add that stirng
+    traverse one row. if start character of a string is contained in that row. add that string
     that column
-rand num. if A mn = null or other place holder. fill it with rand char within bounds of alphabet
+rand num. if A mn = null or other placeholder. fill it with rand char within bounds of alphabet
 print
+ */
